5db4538c86711bc39a5e8a282d2b7b5e61685367
Jesse Mäkinen <jesse.j.makinen@gmail.com>
2024-07-03 23:53:17 +0300

    Fixed a nag about async_forward_entry_setup

--- custom_components/thl_covid/__init__.py
+++ custom_components/thl_covid/__init__.py
@@ -42,7 +42,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = coord
 
-    await hass.config_entries.async_forward_entry_setups(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
 
     return True
 
